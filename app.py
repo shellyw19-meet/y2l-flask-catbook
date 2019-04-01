3ed88bf3883c5e2bcdf5363e41e7e231bc6d061f
aboud20-meet <aboud20@meet.mit.edu>
2019-04-01 18:22:43 +0300

    asdases

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import render_template
-from database import get_all_cats
+from flask import render_template, redirect, url_for, request
+from database import get_all_cats, get_cat_by_id, create_cat, update_votes
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
@@ -8,13 +8,25 @@ app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
 @app.route('/')
 def catbook_home():
     cats = get_all_cats()
-    return render_template("home.html", cats=cats)
+    name = "shelly"
+    return render_template("home.html", cats = cats, name = name)
 
 @app.route('/cats/<int:id>')
 def vote_cats(id):
 	cat = get_cat_by_id(id)
-	return render_template(
-		'cat.html', cat = cat)
+	update_votes(cat)
+	return render_template('cat.html', cat = cat)
+
+@app.route('/add_cat', methods = ['GET', 'POST'])
+def add_cat():
+	if request.method == 'GET':
+		return render_template("add_cat.html")
+	else:
+		name = request.form['name']
+		create_cat(name)
+		return redirect(url_for("catbook_home"))
+
+
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -9,10 +9,18 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 def create_cat(name):
-    cat_object = Cat(name=name)
+    cat_object = Cat(name=name, votes = 0)
     session.add(cat_object)
     session.commit()
 
 def get_all_cats():
     cats = session.query(Cat).all()
     return cats
+
+def get_cat_by_id(id): 
+	cat = session.query(Cat).filter_by(id = id).first()
+	return cat
+
+def update_votes(cat):
+	cat.votes += 1
+	session.commit()
